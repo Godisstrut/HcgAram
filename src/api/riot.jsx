0eb3349d7b5d4fe3dd444967442870e55d3f6764
Godisstrut <erikh13255@gmail.com>
2026-01-15 16:37:10 +0100

    API fetch
    Started working on the API call for getting a summoner

---
+++ src/api/riot.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const RIOT_API_KEY = import.meta.env.VITE_RIOT_API_KEY;
+const RIOT_REGION = "eun1";
+
+export const getSummonerName = async (summonerName) => {
+    
+}
